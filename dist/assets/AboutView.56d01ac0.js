85ed4b7e566fda28015f61ec92a03b1cd05be4fe
KumarGaurav@Codju <gaurav.kumar@codju.com>
2022-11-04 02:30:57 +0530

    deploy

--- dist/assets/AboutView.56d01ac0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o,a as _}from"./index.ea8fc06d.js";const s={},c={class:"about"},n=_("h1",null,"Codju Learning Platform",-1),a=[n];function r(i,d){return t(),o("div",c,a)}const u=e(s,[["render",r]]);export{u as default};
